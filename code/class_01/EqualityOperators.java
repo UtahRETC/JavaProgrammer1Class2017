5c97a8f372faadf57a36561bfd6647b2d46caf21
Marcos Minond <minond.marcos@gmail.com>
2017-09-05 16:12:38 -0600

    equality operator code

---
+++ code/class_01/EqualityOperators.java
@@ -0,0 +1,19 @@
+public class EqualityOperators {
+  public static void main(String[] args) {
+    String stringThree = "3";
+    int intThree = 3;
+    double doubleThree = 3;
+
+    boolean isStringThreeEqualToIntThree =
+      stringThree.equals(intThree);
+
+    boolean isInt3EqualToDouble3 =
+      intThree == doubleThree;
+
+    System.out.println("Is String 3 equal to Int 3? "
+      + isStringThreeEqualToIntThree);
+
+    System.out.println("Is Int 3 equal to Double 3? "
+      + isInt3EqualToDouble3);
+  }
+}
