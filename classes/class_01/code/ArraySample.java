5892ebc2b68bb819e4d7199da467d01b0f9a633a
Marcos Minond <minond.marcos@gmail.com>
2017-09-08 20:18:34 -0600

    array sample code

---
+++ classes/class_01/code/ArraySample.java
@@ -0,0 +1,15 @@
+public class ArraySample {
+  public static void main(String[] args) {
+    String[] myFriends;
+
+    myFriends = new String[3];
+
+    myFriends[0] = "Ryan";
+    myFriends[1] = "Sam";
+    myFriends[2] = "Andi";
+
+    System.out.println(myFriends[0] + " is my friend.");
+    System.out.println(myFriends[1] + " is my friend.");
+    System.out.println(myFriends[2] + " is my friend.");
+  }
+}
