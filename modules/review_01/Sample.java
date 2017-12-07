10566eb9f41d2808cc1960100be35bac25b062f9
Marcos Minond <minond.marcos@gmail.com>
2017-12-06 19:41:31 -0700

    rename

---
+++ modules/review_01/Sample.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Sample {
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    int numberToGuess = ThreadLocalRandom.current().nextInt(-250, 250 + 1);
+
+    System.out.printf("Give me your best guess and press enter: ");
+    int numberGuessed = reader.nextShort();
+
+    System.out.printf("The number you guessed is %d and you were supposed to guess %s.\n", numberToGuess, numberGuessed);
+  }
+}
