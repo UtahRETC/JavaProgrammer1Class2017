bbb99b2380451303fc40f635d9401a3ba5f59324
Andrew Jensen <andrewjensen90@gmail.com>
2018-03-24 12:14:02 -0600

    Remove service worker so API can be seen from the browser

--- projects/project_03_web_server/src/main/resources/src/index.js
+++ projects/project_03_web_server/src/main/resources/src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 // import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+// registerServiceWorker();
